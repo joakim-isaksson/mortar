11237fb42060d5f14f01b790762a5270dacfb4e4
Lauri <late.late@gmail.com>
2016-07-04 18:16:28 +0300

    it rotates

---
+++ Assets/Scripts/CannonRotator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonRotator : MonoBehaviour
+{
+    float angle;
+    bool colliding;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colliding) transform.Rotate(new Vector3(0, 0, Time.deltaTime * 90));
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit: " + other.tag);
+        if (other.tag == "Controller") colliding = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter: " + other.tag);
+        if (other.tag == "Controller") colliding = true;
+    }
+}
