0f2d2075258b685da0f02c4e950812b1e616304c
Sharpy Fangs <Sharpy Fangs>
2016-07-05 01:25:17 +0300

    Added missile and cannon prefabs

---
+++ Assets/RotateToTrajectory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToTrajectory : MonoBehaviour
+{
+	Rigidbody rigidBody;
+
+	void Awake()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rigidBody.MoveRotation(Quaternion.LookRotation(rigidBody.velocity));
+	}
+}
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+	public GameObject prefab;
+	public float force;
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space")) Spawn();
+	}
+
+	public void Spawn()
+	{
+		GameObject missile = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
+		Rigidbody rb = missile.GetComponent<Rigidbody>();
+		rb.AddForce(transform.up * force, ForceMode.Impulse);
+	}
+}
