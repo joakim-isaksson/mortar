07ca380db9c7b3d79657a24bc5e4cda83b540168
Lauri <late.late@gmail.com>
2016-07-05 17:56:13 +0300

    updated rotator

--- Assets/Scripts/CannonRotator.cs
+++ Assets/Scripts/CannonRotator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿#define _DEBUG_CONTROLLER_
+
+using UnityEngine;
 using System.Collections;
 
 public class CannonRotator : MonoBehaviour
@@ -6,17 +8,20 @@ public class CannonRotator : MonoBehaviour
 	float angle;
 	bool grabbed;
 	SteamVR_TrackedObject controller;
+	float nominalDist;
 
 	public Component control;
 	public Component wheel;
 	public Component cannon;
 	public Vector3 axis;
 	public float multiplier;
+	public float maxAngularSpeed;
 
 	// Use this for initialization
 	void Start()
 	{
-
+		Vector3 handleLocalPos = control.transform.InverseTransformPoint(transform.position);
+		nominalDist = new Vector2(handleLocalPos.x, handleLocalPos.y).magnitude;
 	}
 
 	// Update is called once per frame
@@ -24,6 +29,8 @@ public class CannonRotator : MonoBehaviour
 	{
 		if (grabbed)
 		{
+
+#if (!_DEBUG_CONTROLLER_)
 			if (controller.index == SteamVR_TrackedObject.EIndex.None)
 			{
 				grabbed = false;
@@ -38,16 +45,32 @@ public class CannonRotator : MonoBehaviour
 				controller = null;
 				return;
 			}
+#endif
 
 			Vector3 controllerLocalPos = control.transform.InverseTransformPoint(controller.transform.position);
-			Vector2 toControllerDir = new Vector2(controllerLocalPos.x, controllerLocalPos.y).normalized;
+			Vector2 toControllerDir = new Vector2(controllerLocalPos.x, controllerLocalPos.y);
+			float controllerDist = toControllerDir.magnitude;
+			toControllerDir /= controllerDist;
 
-			float angle = Mathf.Rad2Deg * Mathf.Acos(Vector2.Dot(new Vector2(0, 1), toControllerDir));
+			float angleController = Mathf.Rad2Deg * Mathf.Acos(Vector2.Dot(new Vector2(0, 1), toControllerDir));
 			float sign = Mathf.Sign(Vector3.Cross(new Vector3(0, 1, 0), new Vector3(toControllerDir.x, toControllerDir.y, 0)).z);
 
-			float angleDiff = Mathf.DeltaAngle(wheel.transform.eulerAngles.z, angle * sign);
+			float angleDiff = Mathf.DeltaAngle(wheel.transform.eulerAngles.z, angleController * sign);
 
-			wheel.transform.eulerAngles = new Vector3(wheel.transform.eulerAngles.x, wheel.transform.eulerAngles.y, angle * sign);
+			float normAngDiff = angleDiff;
+			//float normAngDiff = Mathf.Max(angleDiff, (angleDiff * controllerDist) / nominalDist);
+
+			float f = Mathf.Clamp(controllerDist / nominalDist, 0, 1);
+			float maxAngSpeedPerFrame = maxAngularSpeed * Time.deltaTime * f;
+			float limitedNormAngDiff = Mathf.Clamp(normAngDiff, -maxAngSpeedPerFrame, maxAngSpeedPerFrame);
+
+			angleDiff = limitedNormAngDiff;
+
+			Debug.Log("speed: " + normAngDiff + ", limited: " + limitedNormAngDiff);
+
+			wheel.transform.eulerAngles = new Vector3(wheel.transform.eulerAngles.x, wheel.transform.eulerAngles.y, wheel.transform.eulerAngles.z + angleDiff);
+
+			//wheel.transform.eulerAngles = new Vector3(wheel.transform.eulerAngles.x, wheel.transform.eulerAngles.y, angleController * sign);
 
 			//Debug.Log("angle: " + angle + ", sign: " + sign + ", euler: " + wheel.transform.eulerAngles.ToString("f4") + ", diff: " + angleDiff);
 
@@ -76,10 +99,10 @@ public class CannonRotator : MonoBehaviour
 			SteamVR_TrackedObject controller = other.GetComponent<SteamVR_TrackedObject>();
 			if (controller != null)
 			{
+#if (!_DEBUG_CONTROLLER_)
 				if (controller.index == SteamVR_TrackedObject.EIndex.None) return;
-				int index = (int)controller.index;
-
-				if (SteamVR_Controller.Input(index).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+				if (SteamVR_Controller.Input((int)controller.index).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+#endif
 				{
 					grabbed = true;
 					this.controller = controller;
