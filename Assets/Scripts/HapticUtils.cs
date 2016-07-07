adde0cd0d022d31d9cb8113d651ae48a05b53646
Lauri <late.late@gmail.com>
2016-07-07 16:17:19 +0300

    fire haptics

--- Assets/Scripts/HapticUtils.cs
+++ Assets/Scripts/HapticUtils.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class HapticUtils
 {
-	// length is how long the vibration should go for
-	// strength is vibration strength from 0-1
-	public static IEnumerator LongVibrationBoth(float length, float strength)
+	public delegate float StrengthFunction(float t);
+
+	public static float MaxStrength(float t) { return 1; }
+
+	public static IEnumerator LongVibrationBoth(float length, StrengthFunction f = null)
 	{
+		// Default to constant function
+		if (f == null) f = MaxStrength;
+
 		int left = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
 		int right = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
 
 		for (float i = 0; i < length; i += Time.deltaTime)
 		{
+			float strength = f(i / length);
+
 			if (left != -1) SteamVR_Controller.Input(left).TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
 			if (right != -1 && left != right) SteamVR_Controller.Input(right).TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
 			yield return null;
