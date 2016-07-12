e4f101fb9569b9bf1341100133fd7e27739f844f
Lauri <late.late@gmail.com>
2016-07-12 13:12:16 +0300

    added debug object

---
+++ Assets/Scripts/DebugObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugObject : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			Debug.Log("space pressed");
+		}
+	}
+}
